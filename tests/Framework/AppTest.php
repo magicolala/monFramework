c8f4ae6f5b9bdba99e0f9baf69d79095b3d3b738
Cédric Oloa <cedric.oloa@laposte.net>
2020-01-31 14:22:13 +0100

    add router

--- tests/Framework/AppTest.php
+++ tests/Framework/AppTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Framework;
 
+use App\Blog\BlogModule;
 use Framework\App;
 use GuzzleHttp\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Tests\Framework\Modules\ErroredModule;
+use Tests\Framework\Modules\StringModule;
 
 class AppTest extends TestCase
 {
@@ -18,11 +22,31 @@ class AppTest extends TestCase
     }
 
     public function testBlog() {
-        $app = new App();
+        $app = new App([BlogModule::class]);
         $request = new ServerRequest('GET', '/blog');
         $response = $app->run($request);
         $this->assertStringContainsString('<h1>Bienvenue sur le blog</h1>', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
+
+        $requestSingle = new ServerRequest('GET', '/blog/article-de-test');
+        $responseSingle = $app->run($requestSingle);
+        $this->assertStringContainsString('<h1>Bienvenue sur l\'article article-de-test</h1>', (string)$responseSingle->getBody());
+        $this->assertEquals(200, $responseSingle->getStatusCode());
+    }
+
+    public function testThrowExceptionIfNoResponseSent() {
+        $app = new App([ErroredModule::class]);
+        $request = new ServerRequest('GET', '/demo');
+        $this->expectException(\Exception::class);
+        $app->run($request);
+    }
+
+    public function testConvertStringToResponse() {
+        $app = new App([StringModule::class]);
+        $request = new ServerRequest('GET', '/demo');
+        $response = $app->run($request);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals('DEMO', (string)$response->getBody());
     }
 
     public function testError404() {
